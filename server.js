1ccd258ba4844b5f37fc7eb87487b19556b38eec
logicalMinds1 <systemuser.logicalmindsit@gmail.com>
2025-06-07 22:22:30 +0530

    c

--- server.js
+++ server.js
@@ -12,8 +12,7 @@ import UploadFile from "./src/Routes/up.js"
 import dotenv from "dotenv"
 dotenv.config();
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+
 
 const app = express();
 const PORT =  process.env.PORT || 8000;
@@ -21,6 +20,7 @@ const PORT =  process.env.PORT || 8000;
 // Middleware 
 app.use(cors());
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 app.use(verifyToken);
 
 
