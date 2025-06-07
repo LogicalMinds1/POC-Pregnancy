7c398933ef32647ff644f004b97c798ab8ea4a73
logicalMinds1 <systemuser.logicalmindsit@gmail.com>
2025-06-07 20:02:17 +0530

    pregnancy-poc

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import dotenv from 'dotenv';
-import connectDB from './config/db.js';
+import connectDB from './src/db.js';
 import cors from 'cors';
 
 import MotherId from "./Routes/motherRoutes.js"
